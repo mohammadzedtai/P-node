7568812b928739eb961c752fbe5784f00e3068b8
taimojammadzed <zaidtaizaid@gmail.com>
2026-05-12 11:09:25 +0530

    Camplet Backend

--- FinalPractice/Backend/server/middleware/uploadMiddleware.js
+++ FinalPractice/Backend/server/middleware/uploadMiddleware.js
@@ -1,19 +1,43 @@
 import multer from "multer";
 import path from "path";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
 
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      Date.now() + path.extname(file.originalname)
-    );
-  },
+const storage = multer.diskStorage({
+
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+
 });
 
-const upload = multer({ storage });
 
-export default upload;  
+const fileFilter = (req, file, cb) => {
+
+    const allowedFile = /jpeg|jpg|png|pdf|doc|docx/;
+
+    const extname = path.extname(file.originalname).toLowerCase();
+
+    if (allowedFile.test(extname)) {
+        cb(null, true);
+    } else {
+        cb(new Error("Only images and documents allowed"), false);
+    }
+
+};
+
+
+export const upload = multer({
+
+    storage,
+
+    fileFilter,
+
+    limits: {
+        fileSize: 5 * 1024 * 1024
+    }
+
+});
